f5b2d0d2471b9dc387411b3423321589cda68de0
Hiro K <hirosdev@gmail.com>
2022-03-25 14:58:23 +0900

    prepare deploy

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ require('colors');
 
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 const connectDB = require('./config/db');
 const { errorHandler } = require('./middlewares/errorMiddleware');
@@ -17,6 +18,17 @@ app.use(morgan('dev'));
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
+
+// Server frontent
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../client/build')));
+	app.get('*', (req, res) =>
+		res.sendFile(
+			path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+		)
+	);
+}
+
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
